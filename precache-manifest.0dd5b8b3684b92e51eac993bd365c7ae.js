5c7a3f07895c08af6d2c00631908d90c0137985b
Simran jeet Singh <simranj674@gmail.com>
2020-06-09 22:52:18 +0530

    Updates

---
+++ precache-manifest.0dd5b8b3684b92e51eac993bd365c7ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbc54d13c7203c8c8128d391e78ec6fb",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "605b995334bf884b4349",
+    "url": "/quiz-app/static/css/main.00c0b01a.chunk.css"
+  },
+  {
+    "revision": "ce5844db562df0ba825c",
+    "url": "/quiz-app/static/js/2.3c22be11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz-app/static/js/2.3c22be11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "605b995334bf884b4349",
+    "url": "/quiz-app/static/js/main.802afb7c.chunk.js"
+  },
+  {
+    "revision": "b0c46374be77d62a9c8c",
+    "url": "/quiz-app/static/js/runtime-main.89e6c66e.js"
+  }
+]);
